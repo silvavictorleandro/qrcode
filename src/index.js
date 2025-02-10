0ad038addc426187517870a6bfe3aae3a1018f23
Victor Silva <silvavictorleandro@outlook.com>
2025-02-10 13:52:47 -0300

    iniciando arquitetura projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import prompt from "prompt";
 
-import mainPrompt from "./prompt/prompt-main.js";
+import mainPrompt from "./prompts/prompt-main.js";
+import createQRcode from "./services/qr-code/create.js";
 
 async function main() {
   prompt.get(mainPrompt, async (err, result) => {
-    if (result.select === 1) {
-      console.log("escolheu o QRCODE");
-    }
+    if (result.select === 1) await createQRcode();
     if (result.select === 2) {
       console.log("escolheu o PASSWORD");
     }
---
+++ src/services/qr-code/create.js
@@ -0,0 +1,6 @@
+import prompt from "prompt";
+import qrcode from "../../prompts/prompt-qrcode.js";
+
+async function createQRcode() {}
+
+export default createQRcode;
