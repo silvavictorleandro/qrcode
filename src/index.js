6be686a64455b6e62f55ca05121b3c4dc910d0f8
Victor Silva <silvavictorleandro@outlook.com>
2025-02-16 13:03:31 -0300

    criacao gerador de senha

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import prompt from "prompt";
-
 import mainPrompt from "./prompts/prompt-main.js";
 import createQRcode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 async function main() {
   prompt.get(mainPrompt, async (err, result) => {
     if (result.select == 1) await createQRcode();
-    if (result.select == 2) {
-      console.log("escolheu o PASSWORD");
-    }
+    if (result.select == 2) await createPassword();
   });
 
   prompt.start();
